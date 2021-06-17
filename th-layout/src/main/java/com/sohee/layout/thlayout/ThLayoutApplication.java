5ed7086de6e715de7f71ac73e1c391cb4f19d3b7
soheeoott <soheeoott@gmail.com>
2021-06-18 00:20:57 +0900

    [setting] 환경 설정

---
+++ th-layout/src/main/java/com/sohee/layout/thlayout/ThLayoutApplication.java
@@ -0,0 +1,11 @@
+package com.sohee.layout.thlayout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThLayoutApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ThLayoutApplication.class, args);
+	}
+}
---
+++ th-layout/src/test/java/com/sohee/layout/thlayout/ThLayoutApplicationTests.java
@@ -0,0 +1,12 @@
+package com.sohee.layout.thlayout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThLayoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
