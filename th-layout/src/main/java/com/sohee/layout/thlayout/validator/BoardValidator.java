7fd8f96006a4b120db48a9fc2d6c072569d028e9
soheeoott <soheeoott@gmail.com>
2021-06-22 23:44:07 +0900

    [Add] ObjectUtils를 사용한 커스텀 검증 클래스

---
+++ th-layout/src/main/java/com/sohee/layout/thlayout/validator/BoardValidator.java
@@ -0,0 +1,23 @@
+package com.sohee.layout.thlayout.validator;
+
+import antlr.StringUtils;
+import com.sohee.layout.thlayout.model.Board;
+import org.springframework.util.ObjectUtils;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+public class BoardValidator implements Validator {
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Board.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object obj, Errors errors) {
+        Board board = (Board) obj; // 클래스로 형변환
+        if (ObjectUtils.isEmpty(board.getContent())){ // StringUtils.isEmpty 대체
+            errors.rejectValue("content", "key", "내용을 입력하세요.");
+        }
+    }
+}
