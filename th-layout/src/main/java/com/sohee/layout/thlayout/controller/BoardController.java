fdfbb28345f4d040acb4708f517f021ffe685980
soheeoott <soheeoott@gmail.com>
2021-06-22 20:54:46 +0900

    [Add] 요청 메소드 방식만 바꿔서 전달해주었던 클래스를 다시 받아옴

--- th-layout/src/main/java/com/sohee/layout/thlayout/controller/BoardController.java
+++ th-layout/src/main/java/com/sohee/layout/thlayout/controller/BoardController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -22,5 +24,17 @@ public class BoardController {
         List<Board> boards = boardRepository.findAll(); // 데이터 전부 가져오기
         model.addAttribute("boards", boards); // 담은 데이터는 thymeleaf 에서 사용
         return "board/list";
+    }
+
+    @GetMapping("/write")
+    public String write(Model model) {
+        model.addAttribute("board", new Board());
+        return "board/write";
+    }
+
+    @PostMapping("/write")
+    public String writeSubmit(@ModelAttribute Board board) {
+        boardRepository.save(board);
+        return "redirect:/board/list";
     }
 }
