24ac7bb18118e83492cf7d48831535ffbe96c304
soheeoott <soheeoott@gmail.com>
2021-06-24 23:18:46 +0900

    [Add] 사용자 테이블(User) 모델

---
+++ th-layout/src/main/java/com/sohee/layout/thlayout/model/User.java
@@ -0,0 +1,26 @@
+package com.sohee.layout.thlayout.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+public class User {
+    // 데이터베이스에서 정의한 필드들을 정의
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userid;
+
+    private String username;
+    private String password;
+    private Boolean enabled;
+
+    @ManyToMany
+    @JoinTable(
+        name = "user_role", // 테이블 이름
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id")) // 조인 할 상대 테이블
+    private List<Role> roles; // 조인
+}
