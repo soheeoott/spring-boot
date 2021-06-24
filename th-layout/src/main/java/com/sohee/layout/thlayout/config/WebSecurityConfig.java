8f9a0857fe5bcbca3b464a706d68cd118f94d3db
soheeoott <soheeoott@gmail.com>
2021-06-24 17:50:11 +0900

    [Add] 로그인 화면

--- th-layout/src/main/java/com/sohee/layout/thlayout/config/WebSecurityConfig.java
+++ th-layout/src/main/java/com/sohee/layout/thlayout/config/WebSecurityConfig.java
@@ -27,7 +27,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
-                .loginPage("/login")
+                .loginPage("/account/login")
                 .permitAll()
                 .and()
             .logout()
