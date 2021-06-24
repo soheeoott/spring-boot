cea4a6ff14050f95f3ea8f37aecb6192a86912db
soheeoott <soheeoott@gmail.com>
2021-06-25 02:33:16 +0900

    [Add] 게시글 작성자에 사용자 연결

--- th-layout/src/main/java/com/sohee/layout/thlayout/model/Board.java
+++ th-layout/src/main/java/com/sohee/layout/thlayout/model/Board.java
@@ -1,13 +1,9 @@
 package com.sohee.layout.thlayout.model;
 
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import lombok.Data;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 @Entity
 @Data
@@ -21,4 +17,8 @@ public class Board {
     @Size(min=2, max=30, message = "제목은 2자 이상 30자 이하입니다.")
     private String title;
     private String content;
+
+    @ManyToOne // 게시글과 사용자와의 관계
+    @JoinColumn(name = "userid", referencedColumnName = "userid")
+    private User user;
 }
