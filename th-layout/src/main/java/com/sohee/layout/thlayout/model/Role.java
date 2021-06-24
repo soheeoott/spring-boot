082f66399c6ba7972bf63a418c481410b614dbbb
soheeoott <soheeoott@gmail.com>
2021-06-24 23:19:15 +0900

    [Add] 권한 테이블(Role) 모델

---
+++ th-layout/src/main/java/com/sohee/layout/thlayout/model/Role.java
@@ -0,0 +1,20 @@
+package com.sohee.layout.thlayout.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+public class Role {
+    // 데이터베이스에서 정의한 필드들을 정의
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long roleid;
+
+    private String rolename;
+
+    @ManyToMany(mappedBy = "roles") // User 클래스에 있는 조인 컬럼
+    private List<User> users;
+}
