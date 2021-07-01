8c3b1f274867901e97062befd0a52f3c5862a9b1
soheeoott <soheeoott@gmail.com>
2021-07-02 03:10:52 +0900

    [refactor] 테스트 코드 출력 방식을 로깅으로 변경

--- Mustache-layout/src/main/java/com/springboot/beginner/controller/CrudController.java
+++ Mustache-layout/src/main/java/com/springboot/beginner/controller/CrudController.java
@@ -3,12 +3,14 @@ package com.springboot.beginner.controller;
 import com.springboot.beginner.dto.Board;
 import com.springboot.beginner.entity.BoardEntity;
 import com.springboot.beginner.repository.BoardRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Slf4j
 public class CrudController {
 
     @Autowired
@@ -21,13 +23,17 @@ public class CrudController {
 
     @PostMapping("/create")
     public String create(Board board) {
-        System.out.println(board.toString());
+        // System.out.println(board.toString()); -> 로깅으로
+        log.info(board.toString());
 
         BoardEntity boardEntity = board.toEntity();
-        System.out.println(boardEntity.toString());
+        // System.out.println(boardEntity.toString()); -> 로깅으로
+        log.info(boardEntity.toString());
 
         BoardEntity saved = boardRepository.save(boardEntity);
-        System.out.println(saved.toString());
+        // System.out.println(saved.toString()); -> 로깅으로
+        log.info(saved.toString());
+
         return "";
     }
 }
