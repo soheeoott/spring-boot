ab44ec6dad65a6fc47b2640bdd6b8b81ae87b3f5
soheeoott <soheeoott@gmail.com>
2021-07-02 02:55:48 +0900

    [refactor] Lombok 라이브러리를 적용한 DTO refactoring

--- Mustache-layout/src/main/java/com/springboot/beginner/dto/Board.java
+++ Mustache-layout/src/main/java/com/springboot/beginner/dto/Board.java
@@ -1,22 +1,14 @@
 package com.springboot.beginner.dto;
 
 import com.springboot.beginner.entity.BoardEntity;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@ToString
 public class Board {
     private String title;
     private String content;
-
-    public Board(String title, String content){
-        this.title = title;
-        this.content = content;
-    }
-    @Override
-    public String toString() {
-        return "Board{" +
-            "title='" + title + '\'' +
-            ", content='" + content + '\'' +
-            '}';
-    }
 
     public BoardEntity toEntity() {
         return new BoardEntity(null, title, content);
