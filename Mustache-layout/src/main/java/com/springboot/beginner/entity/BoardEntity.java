32f6707d89f5981ab84e7d4f61ed7dddb6c35e81
soheeoott <soheeoott@gmail.com>
2021-07-02 02:59:34 +0900

    [refactor] Lombok 라이브러리를 적용한 Entity refactoring

--- Mustache-layout/src/main/java/com/springboot/beginner/entity/BoardEntity.java
+++ Mustache-layout/src/main/java/com/springboot/beginner/entity/BoardEntity.java
@@ -1,11 +1,16 @@
 package com.springboot.beginner.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
+@AllArgsConstructor
+@ToString
 public class BoardEntity {
 
     @Id
@@ -17,19 +22,4 @@ public class BoardEntity {
 
     @Column
     private String content;
-
-    public BoardEntity(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "BoardEntity{" +
-            "id=" + id +
-            ", title='" + title + '\'' +
-            ", content='" + content + '\'' +
-            '}';
-    }
 }
